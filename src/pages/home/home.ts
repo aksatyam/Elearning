fb7221a060772405b991dcba502c8bfe4aa3637a
Ashish Kumar Satyam <ashishsatyam.kmr@gmail.com>
2018-07-01 07:44:52 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,99 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public CardList:any=[]; 
 
   constructor(public navCtrl: NavController) {
+    this.CardListCall();
+  }
 
+  CardListCall(){
+    this.CardList=[
+                    {
+                      textStart:'U1',
+                      textStartBackgroumdColor:'red',
+                      unitName:'Unit 1 Handwritten',
+                      iconName:'ios-share',
+                      authorIcon:'ios-person',
+                      author:'A K Satyam',
+                      uploadDateIcon:'md-calendar',
+                      uploadedDate:new Date().toISOString(),
+                      views:356,
+                      fileSize:'35.00 MB'
+                    },
+                    {
+                      textStart:'U1',
+                      textStartBackgroumdColor:'red',
+                      unitName:'Unit 1 Handwritten',
+                      iconName:'ios-share',
+                      authorIcon:'ios-person',
+                      author:'A K Satyam',
+                      uploadDateIcon:'md-calendar',
+                      uploadedDate:new Date().toISOString(),
+                      views:356,
+                      fileSize:'35.00 MB'
+                    },
+                    {
+                      textStart:'U1',
+                      textStartBackgroumdColor:'red',
+                      unitName:'Unit 1 Handwritten',
+                      iconName:'ios-share',
+                      authorIcon:'ios-person',
+                      author:'A K Satyam',
+                      uploadDateIcon:'md-calendar',
+                      uploadedDate:new Date().toISOString(),
+                      views:356,
+                      fileSize:'35.00 MB'
+                    },
+                    {
+                      textStart:'U1',
+                      textStartBackgroumdColor:'red',
+                      unitName:'Unit 1 Handwritten',
+                      iconName:'ios-share',
+                      authorIcon:'ios-person',
+                      author:'A K Satyam',
+                      uploadDateIcon:'md-calendar',
+                      uploadedDate:new Date().toISOString(),
+                      views:356,
+                      fileSize:'35.00 MB'
+                    },
+                    {
+                      textStart:'U1',
+                      textStartBackgroumdColor:'red',
+                      unitName:'Unit 1 Handwritten',
+                      iconName:'ios-share',
+                      authorIcon:'ios-person',
+                      author:'A K Satyam',
+                      uploadDateIcon:'md-calendar',
+                      uploadedDate:new Date().toISOString(),
+                      views:356,
+                      fileSize:'35.00 MB'
+                    },
+                    {
+                      textStart:'U1',
+                      textStartBackgroumdColor:'red',
+                      unitName:'Unit 1 Handwritten',
+                      iconName:'ios-share',
+                      authorIcon:'ios-person',
+                      author:'A K Satyam',
+                      uploadDateIcon:'md-calendar',
+                      uploadedDate:new Date().toISOString(),
+                      views:356,
+                      fileSize:'35.00 MB'
+                    },
+                    {
+                      textStart:'U1',
+                      textStartBackgroumdColor:'red',
+                      unitName:'Unit 1 Handwritten',
+                      iconName:'ios-share',
+                      authorIcon:'ios-person',
+                      author:'A K Satyam',
+                      uploadDateIcon:'md-calendar',
+                      uploadedDate:new Date().toISOString(),
+                      views:356,
+                      fileSize:'35.00 MB'
+                    }
+                  ]
   }
 
 }
